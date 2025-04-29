f9300414f1aa42889988ac62551fc42506a4adfa
az-marques <85958771+az-marques@users.noreply.github.com>
2025-04-29 15:55:42 -0300

    método joinRoom r6 e r7

--- UserChat.java
+++ UserChat.java
@@ -1,3 +1,5 @@
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -14,5 +16,18 @@ public class UserChat extends UnicastRemoteObject implements IUserChat {
     @Override
     public void deliverMsg(String senderName, String msg) {
         System.out.println("[" + roomName + "] " + senderName + ": " + msg);
+    }
+    
+    public void joinRoom(String roomName){
+        try {
+            IRoomChat room = (IRoomChat) Naming.lookup(roomName);
+            room.joinRoom(userName, this);
+        } catch (NotBoundException e) {
+            serverChat.createRoom(roomName);
+            joinRoom(roomName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        
     }
 }
