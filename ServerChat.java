fa5e9ba5bca8aab77101a11b89f20f1066a38c0c
az-marques <85958771+az-marques@users.noreply.github.com>
2025-04-29 15:05:06 -0300

    added main method to serverxhat

--- ServerChat.java
+++ ServerChat.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
 
 public class ServerChat implements IServerChat {
+
 
     @Override
     public ArrayList<String> getRooms() {
@@ -10,8 +13,18 @@ public class ServerChat implements IServerChat {
 
     @Override
     public void createRoom(String roomName) {
+        
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'createRoom'");
     }
     
+    public static void main(String[] args) {
+        try {
+            ServerChat newServer = new ServerChat();
+            ServerChat stub = (ServerChat) UnicastRemoteObject.exportObject(newServer, 0);
+            Naming.rebind("nomeWIP", stub);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
 }
